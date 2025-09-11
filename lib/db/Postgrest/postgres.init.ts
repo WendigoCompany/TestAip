e543334aa4679965f2d0c16eb8870d800e2aa533
Bonafo Bona <>
2025-09-11 02:06:41 -0300

    ok veamos..

--- lib/db/Postgrest/postgres.init.ts
+++
@@ -1,8 +0,0 @@
-import { sequelize } from "./conection.ts";
-import { dbCheck, initDB } from "./dbControl.ts";
-
-(async () => {
-  await sequelize.query(`CREATE SCHEMA IF NOT EXISTS api;`);
-  await initDB();
-  dbCheck();
-})();
